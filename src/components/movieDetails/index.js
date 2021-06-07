04da02e8a64c37c6211d1ba53155ce6e52735658
Ian Hutchinson <ianhutch90@gmail.com>
2021-06-07 11:23:55 +0100

    Movie Details Page - included production countries

--- src/components/movieDetails/index.js
+++ src/components/movieDetails/index.js
@@ -65,6 +65,16 @@ const MovieDetails = (props) => {
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
 
+      <Paper component="ul" className={classes.root}>
+        <li>
+          <Chip label="Production Countries" className={classes.chip} color="primary" />
+        </li>
+        {movie.production_countries.map((p) => (
+          <li key={p.name}>
+            <Chip label={p.name} className={classes.chip} />
+          </li>
+        ))}
+      </Paper>
       <Fab color="secondary" variant="extended" className={classes.fab}>
         <NavigationIcon />
         Reviews
