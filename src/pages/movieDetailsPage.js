828cf287b74db031440008ad8fb1626f330656c9
Ian Hutchinson <ianhutch90@gmail.com>
2021-06-12 10:13:48 +0100

    Show movie review excerpts; Refactor Details page

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -1,31 +1,16 @@
 import React, { useState, useEffect } from "react";
-import MovieHeader from "../components/headerMovie/";
 import MovieDetails from "../components/movieDetails/";
-import { getMovie, getMovieImages } from "../api/tmdb-api";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
+import PageTemplate from "../components/templateMoviePage";
+import { getMovie } from "../api/tmdb-api";
+import { withRouter } from "react-router-dom";
 
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
-  },
-  gridList: {
-    width: 450,
-    height: "100vh",
-  },
-}));
-
-const MoviePage = (props) => {
-  const classes = useStyles();
+const MovieDetailsPage = (props) => {
   //comnst id allows the component to extract the movie id from the browser's parameterized URL address
   const { id } = props.match.params;
   const [movie, setMovie] = useState(null);
-  const [images, setImages] = useState([]);
+  //movie state variable is initialized to null (boolean false) 
+  //but it is eventually assigned to an object (boolean true)
 
   //uses the API's movie endpoint to get the full details on the movie
   useEffect(() => {
@@ -34,51 +19,20 @@ const MoviePage = (props) => {
     });
   }, [id]);
 
-  useEffect(() => {
-    getMovieImages(id).then((images) => {
-      setImages(images);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+  //In the below  code the children prop will be bound to: <MovieDetails movie={movie} />
   return (
     <>
       {movie ? (
         <>
-          <MovieHeader movie={movie} />
-          <Grid container spacing={5} style={{ padding: "15px" }}>
-            <Grid item xs={3}>
-              <div className={classes.root}>
-                <GridList
-                  cellHeight={500}
-                  className={classes.gridList}
-                  cols={1}
-                >
-                  {images.map((image) => (
-                    <GridListTile
-                      key={image.file_path}
-                      className={classes.gridListTile}
-                      cols={1}
-                    >
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500/${image.file_path}`}
-                        alt={image.file_path}
-                      />
-                    </GridListTile>
-                  ))}
-                </GridList>
-              </div>
-            </Grid>
-            <Grid item xs={9}>
-              <MovieDetails movie={movie} />
-            </Grid>
-          </Grid>
+          <PageTemplate movie={movie}>
+            <MovieDetails movie={movie} />
+          </PageTemplate>
         </>
       ) : (
-        <h2>Waiting for API data</h2>
+        <p>Waiting for movie details</p>
       )}
     </>
   );
 };
 
-export default MoviePage;
+export default withRouter(MovieDetailsPage);
