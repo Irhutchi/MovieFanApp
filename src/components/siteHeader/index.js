2b7321a3fcf818c7de338c9c6e88e46e740406a6
Ian Hutchinson <ianhutch90@gmail.com>
2021-06-12 11:50:18 +0100

    Updated site header

---
+++ src/components/siteHeader/index.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+
+import MenuIcon from "@material-ui/icons/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import { withRouter } from "react-router-dom";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+  offset: theme.mixins.toolbar,
+}));
+
+const SiteHeader = ( { history }) => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  
+    const menuOptions = [
+      { label: "Home", path: "/" },
+      { label: "Favorites", path: "/movies/favorites" },
+      { label: "Option 3", path: "/" },
+      { label: "Option 4", path: "/" },
+    ];
+  
+    const handleMenuSelect = (pageURL) => {
+      history.push(pageURL);
+    };
+  
+    const handleMenu = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    return (
+      <>
+        <AppBar position="fixed" color="secondary">
+          <Toolbar>
+            <Typography variant="h4" className={classes.title}>
+              TMDB Client
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              All you ever wanted to know about Movies!
+            </Typography>
+              {isMobile ? (
+                <>
+                  <IconButton
+                    aria-label="menu"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleMenu}
+                    color="inherit"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={open}
+                    onClose={() => setAnchorEl(null)}
+                  >
+                    {menuOptions.map((opt) => (
+                      <MenuItem
+                        key={opt.label}
+                        onClick={() => handleMenuSelect(opt.path)}
+                      >
+                        {opt.label}
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </>
+              ) : (
+                <>
+                  {menuOptions.map((opt) => (
+                    <Button
+                      key={opt.label}
+                      color="inherit"
+                      onClick={() => handleMenuSelect(opt.path)}
+                    >
+                      {opt.label}
+                    </Button>
+                  ))}
+                </>
+              )}
+          </Toolbar>
+        </AppBar>
+        <div className={classes.offset} />
+      </>
+    );
+  };
+  
+  export default withRouter(SiteHeader);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import SiteHeader from "./components/siteHeader";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
@@ -11,14 +12,7 @@ import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 const App = () => {
   return (
     <BrowserRouter>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/movies/favorites">Favourites</Link>
-        </li>
-      </ul>
+     <SiteHeader />  
       <Switch>
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
